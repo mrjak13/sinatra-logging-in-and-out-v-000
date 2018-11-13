9975f403b25624b331bb0a78a118117010f3df0e
Justin Kenney <justinkenney13@gmail.com>
2018-11-13 20:41:38 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,10 +1,11 @@
 class Helpers
   def self.current_user(session)
-
     User.find(session[:user_id])
   end
 
   def self.is_logged_in?(session)
+    !!session[:user_id]
+
     binding.pry
   end
 end
